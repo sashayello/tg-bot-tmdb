1bf91f927e375fabfd7235d07ef39ace2fcf80f5
sashkayello <sashkayello@gmail.com>
2020-11-08 16:58:06 +0200

    Update: adding new methods to get tv shows

--- api/Tmdb.js
+++ api/Tmdb.js
@@ -13,6 +13,18 @@ const Tmdb = {
         const request = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${token}&language=ru-RU`)
         const json = await request.json()
         return json
+    },
+
+    async getTvId(token, pageId, movieId) {
+        const request = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${token}&language=ru-RU&page=${pageId}`)
+        const json = await request.json()
+        return json.results[movieId].id
+    },
+
+    async getTvDescription(token, id) {
+        const request = await fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${token}&language=ru-RU`)
+        const json = await request.json()
+        return json
     }
 
 }
--- index.js
+++ index.js
@@ -8,10 +8,13 @@ const movieTrailer = require('./api/MovieTrailer')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) =>  {
-    ctx.reply(`Привет, ${ctx.message.from.first_name}!\n\nНажимай на кнопку и я помогу тебе сгенерировать рандомный фильм!`, Markup.keyboard([['Поиск фильма']]).resize().extra())
+    ctx.reply(`👋 Привет, ${ctx.message.from.first_name}!\n\n🤖 Нажимай на кнопку и я помогу тебе сгенерировать рандомный фильм/сериал/тв программу!\n\n💬  /help - команда поможет тебе разобраться как работать с ботом`, Markup.keyboard([['Поиск фильма'], ['Поиск сериала/программы'], ['Помощь']]).resize().extra())
 })
 
-// bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.help((ctx) => {
+    const message = `🤖 Я умный бот который поможет тебе подобрать фильм, сериал или программу на вечер.\n\n📖 Нажимай на определенную кнопку "Поиск фильма" или "Поиск сериала/программы", также можно в ручную написать команду поиска и я смогу подобрать фильм/сериал/программу.\n\n💬 "Поиск фильма" - команда поиска фильма\n\n💬 "Поиск сериала/программы" - команда поиска ТВ шоу\n\n💬 "Поиск сериала" - команда поиска ТВ шоу(равнозначна команде "Поиск сериала/программы")`
+    ctx.reply(message)
+})
 
 bot.on('text', async (ctx) => {
     const { text } = ctx.message
@@ -19,12 +22,14 @@ bot.on('text', async (ctx) => {
 
     const monthes = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря']
 
-    if(textCapitalize === 'Поиск фильма') {
-        const pageId = Math.floor(Math.random() * (500 - 1 + 1)) + 1
-        const movieId = Math.floor(Math.random() * (20 - 1 + 1)) + 1
+    const pageId = Math.floor(Math.random() * (500 - 1 + 1)) + 1
+    const movieId = Math.floor(Math.random() * (20 - 1 + 1)) + 1
 
-        let data = {}
-        let trailer = ''
+    let data = {}
+    let trailer = ''
+
+    if(textCapitalize === 'Поиск фильма') {
+
 
         try {
             const id = await tmdb.getMovieId(process.env.TMDB_API, pageId, movieId)
@@ -56,7 +61,7 @@ bot.on('text', async (ctx) => {
                 title,
                 original_title: original_title ? original_title : 'Оригинальное название не найдено',
                 overview: overview ? overview : 'Описание фильма не найдено',
-                newGenres,
+                newGenres: newGenres ? newGenres : 'Жарны к фильму не определены',
                 release: `${day} ${month} ${year}`,
                 trailer
             }
@@ -70,11 +75,73 @@ bot.on('text', async (ctx) => {
             ctx.reply(random)
 
         } catch(e) {
-            ctx.reply("Мы не смогли подобрать фильм!")
+            ctx.reply("Мы не смогли подобрать фильм! Попробуйте еще раз!")
             console.log(e);
         }
 
 
+    }
+    if(textCapitalize === 'Поиск сериала/программы' || textCapitalize === 'Поиск сериала') {
+
+        try {
+
+            const id = await tmdb.getTvId(process.env.TMDB_API, pageId, movieId)
+            const tvShow = await tmdb.getTvDescription(process.env.TMDB_API, id)
+
+            const { name, overview, poster_path, genres, original_name, first_air_date, last_air_date } = tvShow
+
+            const tvGenres = genres.map(genre => {
+                return genre.name
+            }).join(', ')
+
+            const poster = `http://image.tmdb.org/t/p/w600_and_h900_bestv2${poster_path}`
+
+            const firstAirDate = first_air_date.split('-')
+            const airYear = firstAirDate[0]
+            const airMonth = monthes[parseInt(firstAirDate[1] - 1)]
+            const airDay = firstAirDate[2]
+
+            const lastAirDate = last_air_date.split('-')
+            const lastYear = lastAirDate[0]
+            const lastMonth = monthes[parseInt(lastAirDate[1] - 1)]
+            const lastDay = lastAirDate[2]
+
+
+            try {
+                trailer = await movieTrailer.getTrailer(original_name)
+            } catch(e) {
+                trailer = 'Трейлер к сериалу/программе не найден!'
+                console.log(e);
+            }
+
+            data = {
+                name,
+                original_name: original_name ? original_name : 'Оригинальное название сериала/программы не найдено',
+                overview: overview ? overview : 'Описание сериала/программы не найдено',
+                tvGenres: tvGenres ? tvGenres : "Жарны к сериалу/программе не определены",
+                airDate: first_air_date !== '' ? `${airDay} ${airMonth} ${airYear}` : 'Дата начала сериала/программы не найдено!',
+                lastDate: last_air_date !== '' ? `${lastDay} ${lastMonth} ${lastYear}` : 'Дата окончания сериала/программы не найдно! Возможно проект еще работает!',
+                trailer
+            }
+
+            if(trailer === 'Трейлер к сериалу/программе не найден!') {
+                ctx.replyWithPhoto(poster)
+            }
+
+            const random = `🎬 Название: ${data.name}\n\n💡 Описание фильма: ${data.overview}\n\n🎞 Оригинальное название: ${data.original_name}\n\n✅ Жанр: ${data.tvGenres}\n\n🗓 Дата релиза: ${data.airDate}\n\n🗓 Дата окончания: ${data.lastDate}\n\n📺 Трейлер: ${data.trailer}`
+
+            ctx.reply(random)
+
+
+        } catch(e) {
+            ctx.reply('Мы не смогли подобрать сериал! Попробуйте еще раз!')
+            console.log(e);
+        }
+
+    }
+    if(textCapitalize === 'Помощь') {
+        const message = `🤖 Я умный бот который поможет тебе подобрать фильм, сериал или программу на вечер.\n\n📖 Нажимай на определенную кнопку "Поиск фильма" или "Поиск сериала/программы", также можно в ручную написать команду поиска и я смогу подобрать фильм/сериал/программу.\n\n💬 "Поиск фильма" - команда поиска фильма\n\n💬 "Поиск сериала/программы" - команда поиска ТВ шоу\n\n💬 "Поиск сериала" - команда поиска ТВ шоу(равнозначна команде "Поиск сериала/программы")\n\n💬 /help - помощь работы с Ботом`
+        ctx.reply(message)
     }
 
 
